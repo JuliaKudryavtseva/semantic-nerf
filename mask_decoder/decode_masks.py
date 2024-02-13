56cbd24c4e8eed9fa3a6798964992395e2f140e5
JuliaKudryavtseva <julia.kudr.alex@gmail.com>
2024-02-14 00:24:07 +0300

    mask_decoder/decode_masks.py: inference

--- mask_decoder/decode_masks.py
+++ mask_decoder/decode_masks.py
@@ -9,16 +9,55 @@ import matplotlib.pyplot as plt
 import cv2
 
 import numpy as np
+import gzip
+
 from lang_sam import LangSAM
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
+def resize(large_features, fmap):
+    large_dict = {fi: [] for fi in range(len(fmap.flatten()))}
+    hw = large_features.reshape(-1, 256).shape[0]
+    for i, fi in zip(range(hw), fmap.flatten()):
+        large_dict[fi].append(large_features.reshape(-1, 256)[i][None, ...])
+
+    # one of side is always 64
+    new_dim = int((fmap.max()+1)/64)
+    list_large_f = []
+    for ind in range(new_dim*64):
+        large_f = torch.tensor(
+            np.concatenate(large_dict[ind], axis=0).mean(0)[None, ...]
+        )
+        list_large_f.append(large_f)
+    #choose dims
+    if np.argmax(fmap.shape):
+        new_h, new_w = new_dim, 64
+        fill_h, fill_w = 64-new_dim, 64
+    else:
+        new_h, new_w = 64, new_dim
+        fill_h, fill_w =64, 64-new_dim
+
+    features = torch.cat(list_large_f, dim=0).reshape(new_h, new_w, 256).permute(2, 0, 1).unsqueeze(0)
+    features = torch.cat([features, torch.zeros((1, 256, fill_h, fill_w))], dim=2)
+    return features
+
+# feature_list=[]
+# for ind in range(256):
+#     features = image_features[ind, :, :].detach().numpy()
+#     sam_feature = cv2.resize( features, (64, 64), interpolation = cv2.INTER_AREA )
+
 def visualise_frame_masks(mask):
     # mask = mask[0].cpu().detach().numpy()
     final_mask = Image.fromarray((255*mask).astype('uint8'), mode="L")
     return final_mask
+
+
+def load_npy_gz(filename):
+    with gzip.open(filename, 'rb') as f:
+        array = np.load(f)
+    return array
 
 # parsing args
 def parse_args():
@@ -50,24 +89,21 @@ if __name__ == '__main__':
 
     # ----------- pathes ---------
     # input
-    video_path = os.path.join('dataset', OUTPUT_NAME, 'images')  
+    video_path = os.path.join('renders/test/rgb')  
     frames = sorted(os.listdir(video_path), key=lambda x: int(x.split('.')[0].split('_')[1]))
 
-    feature_path = os.path.join('dataset', OUTPUT_NAME, 'segmentation_results/seg_features') 
-    feature_frames = sorted(os.listdir(feature_path), key=lambda x: int(x.split('_')[1])) 
-    
+    feature_path = os.path.join('renders/test/raw-sam_features') 
+    feature_frames = sorted(os.listdir(feature_path), key=lambda x: int(x.split('.')[0].split('_')[1])) 
 
     # output
     save_path = os.path.join('assets', OUTPUT_NAME, 'vis_prompt', TEXT_PROMPT)   
     os.makedirs(save_path, exist_ok=True)
 
+    fmap = np.load('dataset/teatime/segmentation_results/features_map.npy')
+
     # ---------------------------- 
     
     print('Experiment name: ', args.exp_name, '\nInput path: ', video_path, 'Output path: ', save_path)
-
-    
-    
-
 
     image_list = []
     for frame_pil, frame_feature in tzip(frames, feature_frames):
@@ -76,13 +112,17 @@ if __name__ == '__main__':
 
 
         frame_feature_path = os.path.join(feature_path, frame_feature)
-        image_features = torch.load(frame_feature_path, map_location='cpu')
+        image_features = load_npy_gz(frame_feature_path)
+        # plt.imshow(image_features[ :, :, 4])
+        # plt.savefig(os.path.join('assets', frame_pil))
+       
 
         # RESIZE HERE
-        feature_list=[]
-        for ind in range(256):
-            features = image_features[0, ind, :, :].detach().numpy()
-            sam_feature = cv2.resize( features, (64, 64), interpolation = cv2.INTER_AREA )
+        sam_feature=resize(image_features, fmap)
+        
+        # plt.imshow(sam_feature[0, 4, :, :])
+        # plt.savefig(os.path.join('assets', frame_pil))
+
         
         # Text SAM segmentation
         masks, boxes, phrases, logits = model.predict(image_featues=sam_feature, 
@@ -94,22 +134,22 @@ if __name__ == '__main__':
             H, W = np.array(image_pill).shape[:2]
             mask = np.zeros((H, W))
         else:
-            # ind_rel = torch.argmax(logits)
-            # mask = masks[ind_rel].squeeze().numpy()
-            masks = [mask.squeeze().numpy() for mask in masks]
-            mask = (np.array(masks).mean(axis=0) > 0).astype(int) # concat all masks in one frame
+            ind_rel = torch.argmax(logits)
+            mask = masks[ind_rel].squeeze().numpy()
+            # masks = [mask.squeeze().numpy() for mask in masks]
+            # mask = (np.array(masks).mean(axis=0) > 0).astype(int) # concat all masks in one frame
 
         vis_masks = visualise_frame_masks(mask)
 
-        # if args.separate:
-        #     plt.imshow(vis_masks)
-        #     plt.savefig(os.path.join(save_path, frame_pil))
+        if args.separate:
+            plt.imshow(vis_masks)
+            plt.savefig(os.path.join(save_path, frame_pil))
 
 
         image_list.append(vis_masks)
 
 
 
-    image_list[0].save(
-        os.path.join(save_path, f"{args.text_prompt}.gif"), 
-        save_all=True, append_images=image_list[1:],  duration=400)
+    # image_list[0].save(
+    #     os.path.join(save_path, f"{args.text_prompt}.gif"), 
+    #     save_all=True, append_images=image_list[1:],  duration=400)
